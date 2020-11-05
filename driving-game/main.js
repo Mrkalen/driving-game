3493fc8ee85cad3dd8da889ddcdefda22036a683
Mrkalen <mrkalen@hotmail.com>
2020-11-05 13:30:24 -0800

    completed user-can-turn-car #2

--- driving-game/main.js
+++ driving-game/main.js
@@ -1,1 +1,16 @@
-// var carDirection = {direction: };
+var carDirection = { direction: 'east' };
+
+var $car = document.querySelector('.car');
+
+document.addEventListener('keydown', function () {
+  if (event.key === 'ArrowUp') {
+    carDirection.direction = 'north';
+  } else if (event.key === 'ArrowRight') {
+    carDirection.direction = 'east';
+  } else if (event.key === 'ArrowDown') {
+    carDirection.direction = 'south';
+  } else if (event.key === 'ArrowLeft') {
+    carDirection.direction = 'west';
+  }
+  $car.setAttribute('class', 'car ' + carDirection.direction);
+});
